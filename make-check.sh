334b0e3c69199e01ebc77c437b08b70b87c71fb3
Mao Han <han_mao@c-sky.com>
2018-12-03 10:01:31 +0800

    store output in log file

--- make-check.sh
+++ make-check.sh
@@ -1,5 +1,7 @@
 rm ~/.ssh/known_hosts -f
 sleep 60
 ssh -o StrictHostKeyChecking=no 172.16.170.121 ls
-make test-wrapper="`pwd`/../scripts/cross-test-ssh.sh --timeoutfactor 6000 172.16.170.121" check > make_check.log 2>&1
+cd output/build/glibc/build
+make test-wrapper="`pwd`/../scripts/cross-test-ssh.sh --timeoutfactor 6000 172.16.170.121" check > make_check_target.log 2>&1
 ssh 172.16.170.121 poweroff
+tail make_check_target.log
