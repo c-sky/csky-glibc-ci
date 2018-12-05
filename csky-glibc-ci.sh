33f93591de91dc8fbba1654fcd5ddec14fd26465
Mao Han <han_mao@c-sky.com>
2018-12-05 08:57:17 +0800

    fix dtb name

--- csky-glibc-ci.sh
+++ csky-glibc-ci.sh
@@ -9,4 +9,4 @@ make > make.log
 make check > make_check.log
 cd -
 output/host/csky-glibc-ci/make-check.sh&
-output/host/csky-qemu/bin/qemu-system-cskyv2 -machine virt -cpu ck810f -kernel output/images/vmlinux -dtb output/images/qemu.dtb -nographic -net nic -net tap,ifname=tap11,script=no
+output/host/csky-qemu/bin/qemu-system-cskyv2 -machine virt -cpu ck810f -kernel output/images/vmlinux -dtb output/images/qemu_glibc_ci.dtb -nographic -net nic -net tap,ifname=tap11,script=no
