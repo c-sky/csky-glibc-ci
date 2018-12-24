9bb191429692cd8fb256703b0261a26e52535e24
Liu Yibin <yibin_liu@c-sky.com>
2018-12-24 17:13:06 +0800

    Fix bugs: rm /root/.ssh/known_host
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- make-check.sh
+++ make-check.sh
@@ -1,5 +1,7 @@
+rm ~/.ssh/known_hosts -f
 sleep 60
+ssh -o StrictHostKeyChecking=no 172.16.170.233 ls
 cd output/build/glibc/build
 make test-wrapper="`pwd`/../scripts/cross-test-ssh.sh --timeoutfactor 6000 172.16.170.233" check > make_check_target.log 2>&1
-ssh 172.16.170.233 -o StrictHostKeyChecking=no -o ServerAliveCountMax=3 -o ServerAliveInterval=1 poweroff -f
+ssh 172.16.170.233 -o ServerAliveCountMax=3 ServerAliveInterval=1 poweroff -f
 tail make_check_target.log
