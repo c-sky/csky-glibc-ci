3aab904d77f0c7e0d40a9fa565b372e0daab42d7
Liu Yibin <yibin_liu@c-sky.com>
2018-12-24 11:05:30 +0800

    ssh poweroff -f
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- make-check.sh
+++ make-check.sh
@@ -1,7 +1,5 @@
-rm ~/.ssh/known_hosts -f
 sleep 60
-ssh -o StrictHostKeyChecking=no 172.16.170.233 ls
 cd output/build/glibc/build
 make test-wrapper="`pwd`/../scripts/cross-test-ssh.sh --timeoutfactor 6000 172.16.170.233" check > make_check_target.log 2>&1
-ssh 172.16.170.233 poweroff
+ssh 172.16.170.233 -o StrictHostKeyChecking=no -o ServerAliveCountMax=3 -o ServerAliveInterval=1 poweroff -f
 tail make_check_target.log
