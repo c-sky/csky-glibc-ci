2f5f454ee6535295a37a95fa23f8debe06816689
Liu Yibin <yibin_liu@c-sky.com>
2019-01-02 16:50:56 +0800

    Fix ip
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- make-check.sh
+++ make-check.sh
@@ -1,7 +1,7 @@
 rm ~/.ssh/known_hosts -f
 sleep 60
-ssh -o StrictHostKeyChecking=no 192.168.100.2 ls
+ssh -o StrictHostKeyChecking=no 192.168.100.4 ls
 cd output/build/glibc/build
-make test-wrapper="`pwd`/../scripts/cross-test-ssh.sh --timeoutfactor 6000 192.168.100.2" check > make_check_target.log 2>&1
-ssh 192.168.100.2 -o ServerAliveCountMax=3 -o ServerAliveInterval=1 poweroff -f
+make test-wrapper="`pwd`/../scripts/cross-test-ssh.sh --timeoutfactor 6000 192.168.100.4" check > make_check_target.log 2>&1
+ssh 192.168.100.4 -o ServerAliveCountMax=3 -o ServerAliveInterval=1 poweroff -f
 tail make_check_target.log
